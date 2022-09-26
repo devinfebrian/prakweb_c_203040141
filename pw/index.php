7bd044600e0da5d830c43334fb5560ef6b626cb9
Devin Febrian <devinfebrian20@gmail.com>
2022-09-26 11:58:15 +0700

    merubah judul website

--- pw/index.php
+++ pw/index.php
@@ -22,12 +22,12 @@ $buku = $rows;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Daftar Buku</title>
+  <title>Daftar Novel</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
 <body>
-  <h3>Daftar Buku</h3>
+  <h3>Daftar Novel</h3>
   <table class="table">
     <thead>
       <tr>
