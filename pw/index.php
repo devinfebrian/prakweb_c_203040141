4a568d8a3c588e2fc624b444ebbbc0bd6b41d8c3
Devin Febrian <devinfebrian20@gmail.com>
2022-09-26 11:43:50 +0700

    Initial commit

---
+++ pw/index.php
@@ -0,0 +1,52 @@
+<?php
+// koneksi DB
+$conn = mysqli_connect('localhost', 'root', '', 'prakweb_c_203040141_pw');
+
+// query isi tabel
+$result = mysqli_query($conn, "SELECT * FROM buku");
+
+// data ke array
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+
+// tampung
+$buku = $rows;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Buku</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+  <h3>Daftar Buku</h3>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Id Buku</th>
+        <th scope="col">Nama Buku</th>
+        <th scope="col">Nama Pengarang</th>
+        <th scope="col">Gambar Buku</th>
+      </tr>
+    </thead>
+    <?php foreach ($buku as $b) : ?>
+      <tbody>
+        <tr>
+          <th scope="row"><?= $b['id_buku']; ?></th>
+          <td><?= $b['nama_buku']; ?></td>
+          <td><?= $b['nama_pengarang']; ?></td>
+          <td><img src="img/<?= $b['gambar_buku']; ?>" width="70"></td>
+      </tbody>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
